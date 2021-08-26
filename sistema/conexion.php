b13f637d466a5ab926a0cd48d65c63f4dc24b1a7
Jorge Cock <jorgecock@gmail.com>
2021-08-26 10:38:18 -0500

    Correccion en password luego de corregir scripts

--- sistema/conexion.php
+++ sistema/conexion.php
@@ -1,8 +1,8 @@
 <?php
-	$host='localhost';  //$host='us-cdbr-east-04.cleardb.com'; //
-	$user='root'; //$user='bc0d9b171404d1'; //
-	$password=''; //$password='8faae0fb'; //  
-	$db='controldeestados1'; //$db='heroku_571592fad775d36'; // 
+	$host='us-cdbr-east-04.cleardb.com'; // $host='localhost';  //
+	$user='bc0d9b171404d1'; // $user='root'; //
+	$password='8faae0fb'; //  $password=''; //
+	$db='heroku_571592fad775d36'; // $db='controldeestados1'; //
 
 	$conexion= mysqli_connect($host,$user,$password,$db);
 	if(!$conexion){
