ee4c9ac9a7e7b6524fb7f5f2a190af56f38507b2
Jorge Cock <jorgecock@gmail.com>
2021-08-26 09:44:57 -0500

    Correccion conexion

--- sistema/conexion.php
+++ sistema/conexion.php
@@ -1,8 +1,8 @@
 <?php
-	$host='127.0.0.1';
-	$user='root';
-	$password='';
-	$db='controldeestados1';
+	$host='us-cdbr-east-04.cleardb.com'; //$host='localhost'
+	$user='bc0d9b171404d1'; //$user='root'
+	$password='8faae0fb'; // $password=''; //
+	$db='heroku_571592fad775d36'; //$db='controldeestados1';
 
 	$conexion= mysqli_connect($host,$user,$password,$db);
 	if(!$conexion){
@@ -11,3 +11,4 @@
 		//echo "ok";
 	}
 ?>
+
